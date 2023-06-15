71ea40331adfdbc8af832c6130df0c4bb5649f0a
UweSilver <40294263+syutotyou@users.noreply.github.com>
2023-06-15 17:48:41 +0900

    render normal

--- ray_tracing_weekend/src/main.rs
+++ ray_tracing_weekend/src/main.rs
@@ -89,19 +89,26 @@ impl Point for Ray{
     }
 }
 
-fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> bool{
+fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> f64{
     let oc = ray.origin - center;
     let a = dot(ray.dir, ray.dir);
     let b = 2.0 * dot(oc, ray.dir);
     let c = dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - f64::sqrt(discriminant)) / (2.0 * a)
+    }
 }
 
 fn ray_colour(ray: Ray) -> Colour{
-    if hit_sphere(Point3{x: 0.0, y: 0.0, z: -1.0}, 0.5, ray){
-        return Colour{x: 1.0, y: 0.0, z: 0.0}
+    let t =  hit_sphere(Point3{x: 0.0, y: 0.0, z: -1.0}, 0.5, ray);
+    if t > 0.0{
+        let n = unit_vector(ray.at(t) - Vec3{x: 0.0, y: 0.0, z: -1.0});
+        return 0.5 * Colour{x: n.x + 1.0, y: n.y + 1.0, z: n.z + 1.0};
     }
+
     let unit_direction: Vec3 = unit_vector(ray.dir);
     let t = 0.5 * (unit_direction.y + 1.0);
     (1.0 - t) * Colour{x: 1.0, y: 1.0, z: 1.0} + t * Colour{x: 0.5, y: 0.7, z: 1.0}
