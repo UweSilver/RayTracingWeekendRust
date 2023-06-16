fb76e9f826dbed0624653f5acbfaa8afe2b4cca0
uwe <40294263+UweSilver@users.noreply.github.com>
2023-06-16 21:10:07 +0900

    add camera and format

--- ray_tracing_weekend/src/main.rs
+++ ray_tracing_weekend/src/main.rs
@@ -1,76 +1,105 @@
-use std::{ops, default};
+use rand::prelude::*;
+use std::{default, ops};
 
 #[derive(Copy, Debug, Clone)]
-struct Vec3{
+struct Vec3 {
     x: f64,
     y: f64,
     z: f64,
 }
 
-impl Default for Vec3{
+impl Default for Vec3 {
     fn default() -> Self {
-        Vec3 { x: 0.0, y: 0.0, z: 0.0 }
+        Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
     }
 }
 
-impl ops::Add<Vec3> for Vec3{
+impl ops::Add<Vec3> for Vec3 {
     type Output = Vec3;
-    fn add(self, rhs: Vec3) -> Vec3{
-        let output: Vec3 = Vec3 { x: (self.x + rhs.x), y: (self.y + rhs.y), z: (self.z + rhs.z) };
+    fn add(self, rhs: Vec3) -> Vec3 {
+        let output: Vec3 = Vec3 {
+            x: (self.x + rhs.x),
+            y: (self.y + rhs.y),
+            z: (self.z + rhs.z),
+        };
         output
     }
 }
 
-impl ops::Sub<Vec3> for Vec3{
+impl ops::Sub<Vec3> for Vec3 {
     type Output = Vec3;
-    fn sub(self, rhs: Vec3) -> Vec3{
-        let output: Vec3 = Vec3 { x: (self.x - rhs.x), y: (self.y - rhs.y), z: (self.z - rhs.z) };
+    fn sub(self, rhs: Vec3) -> Vec3 {
+        let output: Vec3 = Vec3 {
+            x: (self.x - rhs.x),
+            y: (self.y - rhs.y),
+            z: (self.z - rhs.z),
+        };
         output
     }
 }
 
-impl ops::Mul<f64> for Vec3{
+impl ops::Mul<f64> for Vec3 {
     type Output = Vec3;
-    fn mul(self, _rhs: f64) -> Self::Output{
-        Self::Output{x: self.x * _rhs, y: self.y * _rhs, z: self.z * _rhs}
+    fn mul(self, _rhs: f64) -> Self::Output {
+        Self::Output {
+            x: self.x * _rhs,
+            y: self.y * _rhs,
+            z: self.z * _rhs,
+        }
     }
 }
 
-impl ops::Mul<Vec3> for f64{
+impl ops::Mul<Vec3> for f64 {
     type Output = Vec3;
     fn mul(self, _rhs: Vec3) -> Self::Output {
-        Self::Output{x: self * _rhs.x, y: self * _rhs.y, z: self * _rhs.z}
+        Self::Output {
+            x: self * _rhs.x,
+            y: self * _rhs.y,
+            z: self * _rhs.z,
+        }
     }
 }
 
-impl ops::Div<f64> for Vec3{
+impl ops::Div<f64> for Vec3 {
     type Output = Vec3;
-    fn div(self, _rhs: f64) -> Self::Output{
-        Self::Output{x: self.x / _rhs, y: self.y / _rhs, z: self.z / _rhs}
+    fn div(self, _rhs: f64) -> Self::Output {
+        Self::Output {
+            x: self.x / _rhs,
+            y: self.y / _rhs,
+            z: self.z / _rhs,
+        }
     }
 }
 
-fn dot(lhs : Vec3, rhs: Vec3) -> f64{
+fn dot(lhs: Vec3, rhs: Vec3) -> f64 {
     lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z
 }
 
-fn cross(lhs : Vec3, rhs: Vec3) -> Vec3{
-    Vec3{x: lhs.y * rhs.z - lhs.z *rhs.y, y: lhs.z * rhs.x - lhs.x * rhs.z, z: lhs.x * rhs.y - lhs.y * rhs.x}
+fn cross(lhs: Vec3, rhs: Vec3) -> Vec3 {
+    Vec3 {
+        x: lhs.y * rhs.z - lhs.z * rhs.y,
+        y: lhs.z * rhs.x - lhs.x * rhs.z,
+        z: lhs.x * rhs.y - lhs.y * rhs.x,
+    }
 }
 
-fn length_squared(lhs: Vec3) -> f64{
+fn length_squared(lhs: Vec3) -> f64 {
     lhs.x * lhs.x + lhs.y * lhs.y + lhs.z * lhs.z
 }
 
-fn length(lhs: Vec3) -> f64{
+fn length(lhs: Vec3) -> f64 {
     f64::sqrt(length_squared(lhs))
 }
 
-fn unit_vector(lhs: Vec3) -> Vec3{
+fn unit_vector(lhs: Vec3) -> Vec3 {
     lhs / length(lhs)
 }
 
-impl std::fmt::Display for Vec3{
+impl std::fmt::Display for Vec3 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{} {} {}", self.x, self.y, self.z)
     }
@@ -79,27 +108,32 @@ impl std::fmt::Display for Vec3{
 type Colour = Vec3;
 type Point3 = Vec3;
 
-fn write_colour(c: Colour){
-    println!("{} {} {}", (255.999 * c.x) as i32, (255.999 * c.y) as i32, (255.999 * c.z) as i32);
+fn write_colour(c: Colour) {
+    println!(
+        "{} {} {}",
+        (255.999 * c.x) as i32,
+        (255.999 * c.y) as i32,
+        (255.999 * c.z) as i32
+    );
 }
 
 #[derive(Copy, Debug, Clone)]
-struct Ray{
+struct Ray {
     origin: Point3,
-    dir: Vec3
+    dir: Vec3,
 }
 
 trait Point {
     fn at(self, t: f64) -> Point3;
 }
 
-impl Point for Ray{
-    fn at(self, t: f64) -> Point3{
+impl Point for Ray {
+    fn at(self, t: f64) -> Point3 {
         self.origin + t * self.dir
     }
 }
 
-fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> f64{
+fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> f64 {
     let oc = ray.origin - center;
     let a = length_squared(ray.dir);
     let half_b = dot(oc, ray.dir);
@@ -113,34 +147,38 @@ fn hit_sphere(center: Point3, radius: f64, ray: Ray) -> f64{
 }
 
 #[derive(Clone, Copy, Debug)]
-struct HitRecord{
+struct HitRecord {
     p: Point3,
     normal: Vec3,
     t: f64,
 }
 
-impl Default for HitRecord{
+impl Default for HitRecord {
     fn default() -> Self {
-        HitRecord { p: Vec3::default(), normal: Vec3::default(), t: f64::default() }
+        HitRecord {
+            p: Vec3::default(),
+            normal: Vec3::default(),
+            t: f64::default(),
+        }
     }
 }
 
-trait Hittable{
-    fn hit(&self, ray: Ray, t_min: f64, t_max: f64)->Option<HitRecord>;
+trait Hittable {
+    fn hit(&self, ray: Ray, t_min: f64, t_max: f64) -> Option<HitRecord>;
 }
 
-struct Sphere{
+struct Sphere {
     center: Point3,
     radius: f64,
 }
 
-impl Hittable for Sphere{
-    fn hit(&self, ray: Ray, t_min: f64, t_max: f64)->Option<HitRecord> {
+impl Hittable for Sphere {
+    fn hit(&self, ray: Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
         let oc = ray.origin - self.center;
         let a = length_squared(ray.dir);
         let half_b = dot(oc, ray.dir);
         let c = length_squared(oc) - self.radius * self.radius;
-        let discriminant = half_b * half_b - a*c;
+        let discriminant = half_b * half_b - a * c;
 
         if discriminant > 0.0 {
             let root = f64::sqrt(discriminant);
@@ -149,62 +187,136 @@ impl Hittable for Sphere{
                 let t = temp;
                 let p = ray.at(t);
                 let normal = (p - self.center) / self.radius;
-                return Some(HitRecord{t: temp, p: p, normal: normal});
+                return Some(HitRecord {
+                    t: temp,
+                    p: p,
+                    normal: normal,
+                });
             }
             let temp = (-half_b + root) / a;
-            if temp < t_max && temp > t_min{
+            if temp < t_max && temp > t_min {
                 let t = temp;
                 let p = ray.at(t);
                 let normal = (p - self.center) / self.radius;
-                return Some(HitRecord{t: t, p: p, normal: normal});
+                return Some(HitRecord {
+                    t: t,
+                    p: p,
+                    normal: normal,
+                });
             }
         }
-        return None
+        return None;
     }
 }
 
-
-struct  HittableList{
+struct HittableList {
     objects: Vec<Box<dyn Hittable>>,
 }
 
-impl Hittable for HittableList{
-    fn hit(&self, ray: Ray, t_min: f64, t_max: f64)->Option<HitRecord> {
+impl Hittable for HittableList {
+    fn hit(&self, ray: Ray, t_min: f64, t_max: f64) -> Option<HitRecord> {
         let mut record = HitRecord::default();
         let mut hit_anithing = false;
         let mut closest_so_far = t_max;
 
-        self.objects.iter().for_each(|object| {
-            match object.hit(ray, t_min, closest_so_far) {
+        self.objects
+            .iter()
+            .for_each(|object| match object.hit(ray, t_min, closest_so_far) {
                 Some(object_hit_record) => {
                     record = object_hit_record.clone();
                     hit_anithing = true;
                     closest_so_far = object_hit_record.t;
                 }
-                None =>{}
-            }
-        });
+                None => {}
+            });
 
-        if hit_anithing{
+        if hit_anithing {
             Some(record)
-        }
-        else{
+        } else {
             None
         }
     }
 }
 
-fn ray_colour(ray: Ray, hittable: Box<&dyn Hittable>) -> Colour{
-    match hittable.hit(ray, 0.0, f64::MAX){
-        Some(record) =>{
-            return 0.5 * (record.normal + Colour{x: 1.0, y: 1.0, z: 1.0});
+struct Camera {
+    origin: Point3,
+    lower_left_corner: Point3,
+    horizontal: Vec3,
+    vertical: Vec3,
+}
+
+impl Default for Camera {
+    fn default() -> Self {
+        let aspect_ratio = 16.0 / 9.0;
+        let viewport_height = 2.0;
+        let viewport_width = aspect_ratio * viewport_height;
+        let focal_length = 1.0;
+
+        let mut camera = Camera {
+            origin: Point3 {
+                x: 0.0,
+                y: 0.0,
+                z: 0.0,
+            },
+            horizontal: Vec3 {
+                x: viewport_width,
+                y: 0.0,
+                z: 0.0,
+            },
+            vertical: Vec3 {
+                x: 0.0,
+                y: viewport_height,
+                z: 0.0,
+            },
+            lower_left_corner: Vec3::default(),
+        };
+        camera.lower_left_corner = camera.origin
+            - camera.horizontal / 2.0
+            - Vec3 {
+                x: 0.0,
+                y: 0.0,
+                z: focal_length,
+            };
+
+        camera
+    }
+}
+
+fn random() -> f64 {
+    rand::random()
+}
+
+fn random_range(min: f64, max: f64) -> f64 {
+    min + (max - min) * random()
+}
+
+fn ray_colour(ray: Ray, hittable: Box<&dyn Hittable>) -> Colour {
+    match hittable.hit(ray, 0.0, f64::MAX) {
+        Some(record) => {
+            return 0.5
+                * (record.normal
+                    + Colour {
+                        x: 1.0,
+                        y: 1.0,
+                        z: 1.0,
+                    });
         }
-        None=> {}
+        None => {}
     }
 
     let unit_direction: Vec3 = unit_vector(ray.dir);
     let t = 0.5 * (unit_direction.y + 1.0);
-    (1.0 - t) * Colour{x: 1.0, y: 1.0, z: 1.0} + t * Colour{x: 0.5, y: 0.7, z: 1.0}
+    (1.0 - t)
+        * Colour {
+            x: 1.0,
+            y: 1.0,
+            z: 1.0,
+        }
+        + t * Colour {
+            x: 0.5,
+            y: 0.7,
+            z: 1.0,
+        }
 }
 
 fn main() {
@@ -220,20 +332,61 @@ fn main() {
     let viewport_width = aspect_ratio * viewport_height;
     let focal_length = 1.0;
 
-    let origin = Point3{x: 0.0, y: 0.0, z: 0.0};
-    let horizontal = Vec3{x: viewport_width, y: 0.0, z: 0.0};
-    let vertical = Vec3{x: 0.0, y: viewport_height, z: 0.0};
-    let lower_left_corner = origin - horizontal / 2.0 - vertical / 2.0- Vec3{x: 0.0, y: 0.0, z: focal_length};
+    let origin = Point3 {
+        x: 0.0,
+        y: 0.0,
+        z: 0.0,
+    };
+    let horizontal = Vec3 {
+        x: viewport_width,
+        y: 0.0,
+        z: 0.0,
+    };
+    let vertical = Vec3 {
+        x: 0.0,
+        y: viewport_height,
+        z: 0.0,
+    };
+    let lower_left_corner = origin
+        - horizontal / 2.0
+        - vertical / 2.0
+        - Vec3 {
+            x: 0.0,
+            y: 0.0,
+            z: focal_length,
+        };
 
-    let world = HittableList{objects: vec![Box::new(Sphere{center: Vec3 { x: 0.0, y: 0.0, z: -1.0 }, radius: 0.5}), Box::new(Sphere{center: Vec3{x: 0.0, y: -100.5, z: -1.0}, radius: 100.0})]};
+    let world = HittableList {
+        objects: vec![
+            Box::new(Sphere {
+                center: Vec3 {
+                    x: 0.0,
+                    y: 0.0,
+                    z: -1.0,
+                },
+                radius: 0.5,
+            }),
+            Box::new(Sphere {
+                center: Vec3 {
+                    x: 0.0,
+                    y: -100.5,
+                    z: -1.0,
+                },
+                radius: 100.0,
+            }),
+        ],
+    };
 
-    for j in (0..image_height).rev(){
+    for j in (0..image_height).rev() {
         eprintln!("scanlines remaining: {0}", j);
-        for i in 0..image_width{
+        for i in 0..image_width {
             let u = i as f64 / (image_width - 1) as f64;
             let v = j as f64 / (image_height - 1) as f64;
 
-            let r = Ray{origin: origin, dir: lower_left_corner + u * horizontal + v * vertical - origin};
+            let r = Ray {
+                origin: origin,
+                dir: lower_left_corner + u * horizontal + v * vertical - origin,
+            };
             let pixel_colour = ray_colour(r, Box::new(&world));
             write_colour(pixel_colour);
         }
